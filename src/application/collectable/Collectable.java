4b6b33e34d1e074ec3092cf71b3bcb123e98d74d
Gerner Balázs <Gerner@DESKTOP-870G18F.local>
2017-09-05 21:27:37 +0200

    New icon set

--- src/application/collectable/Collectable.java
+++ src/application/collectable/Collectable.java
@@ -1,20 +1,40 @@
 package application.collectable;
 
+import java.util.Properties;
+
 import application.CanvasPainter;
+import application.Main;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 public abstract class Collectable {
 
   protected static final double ELEMENT_WIDTH = CanvasPainter.ELEMENT_WIDTH;
-  
+
   protected static final double ELEMENT_HEIGHT = CanvasPainter.ELEMENT_HEIGHT;
-    
+
   protected Color color;
+
+  protected Properties properties;
+
+  protected Image image;
+
+  public Collectable() {
+    properties = new Properties();
+    try {
+      properties.load(Main.class.getResourceAsStream("../resources/images.properties"));
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
   public abstract void collect();
 
-  public abstract void paint(GraphicsContext ctx, Point2D point);
+  public void paint(GraphicsContext ctx, Point2D point) {
+    ctx.drawImage(image, point.getX(), point.getY());
+  }
 
 }
--- src/application/collectable/Poison.java
+++ src/application/collectable/Poison.java
@@ -7,12 +7,11 @@ import application.Manager;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 
 public class Poison extends Collectable {
 
   enum PoisonColor {
-    YELLOW, ORANGE, RED, SPECIAL
+    YELLOW, BLUE, RED
   }
 
   private PoisonColor type;
@@ -20,11 +19,11 @@ public class Poison extends Collectable {
   private Image poisonImg;
 
   public Poison() {
+    super();
     int ordinal = ThreadLocalRandom.current().nextInt(PoisonColor.values().length);
     type = PoisonColor.values()[ordinal];
-    if (type == PoisonColor.SPECIAL) {
-      poisonImg = new Image(Main.class.getResourceAsStream("../resources/special_poison.png"), ELEMENT_WIDTH, ELEMENT_HEIGHT, true, true);
-    }
+    String imgKey = properties.getProperty("poison_" + type.name().toLowerCase());
+    poisonImg = new Image(Main.class.getResourceAsStream(imgKey), ELEMENT_WIDTH, ELEMENT_HEIGHT, true, true);
   }
 
   @Override
@@ -35,38 +34,6 @@ public class Poison extends Collectable {
 
   @Override
   public void paint(GraphicsContext ctx, Point2D point) {
-    final double x = point.getX();
-    final double y = point.getY();
-
-    switch (type) {
-    case YELLOW:
-      this.color = Color.YELLOW;
-      break;
-    case ORANGE:
-      this.color = Color.ORANGE;
-      break;
-    case RED:
-      this.color = Color.RED;
-      break;
-    default:
-      this.color = null; // SPECIAL
-    }
-
-    if (type == PoisonColor.SPECIAL) {
-      ctx.drawImage(poisonImg, x, y);
-    } else {
-      // draw BG
-      ctx.setFill(color);
-      ctx.fillRoundRect(x, y, ELEMENT_WIDTH, ELEMENT_HEIGHT, ELEMENT_WIDTH / 2, ELEMENT_HEIGHT / 2);
-
-      // draw "X"
-      ctx.setStroke(Color.BLACK);
-      double lineWidth = (double) ELEMENT_WIDTH / 6.66;
-      ctx.setLineWidth(lineWidth);
-      ctx.strokeLine(x + ELEMENT_WIDTH / 4, y + ELEMENT_HEIGHT / 4, x + ELEMENT_WIDTH - ELEMENT_WIDTH / 4, y + ELEMENT_HEIGHT - ELEMENT_HEIGHT / 4);
-      ctx.strokeLine(x + ELEMENT_WIDTH / 4, y + ELEMENT_HEIGHT - ELEMENT_HEIGHT / 4, x + ELEMENT_WIDTH - ELEMENT_WIDTH / 4, y + ELEMENT_HEIGHT / 4);
-    }
-
+    ctx.drawImage(poisonImg, point.getX(), point.getY());
   }
-
 }
